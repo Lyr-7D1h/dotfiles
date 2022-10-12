e24d2ae233d89022577f717a5694a94b6f411229
Lyr <lyr-7d1h@pm.me>
2022-10-12 21:38:20 +0200

    started work on google_calendar_next_event

--- .config/nvim/lua/mylsp.lua
+++ .config/nvim/lua/mylsp.lua
@@ -152,7 +152,7 @@ for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     on_attach = function(client, bufnr)
       if client.name == "tsserver" then -- disable formatting for tsserver
-        client.resolved_capabilities.document_formatting = false
+        client.server_capabilities.document_formatting = false
       end
       on_attach(client, bufnr)
     end,
