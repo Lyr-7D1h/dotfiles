850e92c1fa928d43fd31715829d8cae68609972a
Lyr <lyr-7d1h@pm.me>
2023-11-18 03:51:18 +0100

    added remove all buffers keybind and html highlight

--- .config/wireplumber/bluetooth.lua.d/51-bluez-config.lua
+++ .config/wireplumber/bluetooth.lua.d/51-bluez-config.lua
@@ -1,5 +1,5 @@
 bluez_monitor.properties = {
-	["bluez5.enable-sbc-xq"] = true,
-	["bluez5.enable-msbc"] = true,
-	["bluez5.codecs"] = "[sbc sbc_xq]",
+  ["bluez5.enable-sbc-xq"] = true,
+  ["bluez5.enable-msbc"] = true,
+  ["bluez5.codecs"] = "[sbc sbc_xq aac ldac aptx aptx_hd]",
 }
