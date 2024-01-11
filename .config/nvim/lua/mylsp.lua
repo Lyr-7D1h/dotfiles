1bb1f31f6e3c3859f1461a7006c071555161fd40
Lyr <lyr-7d1h@pm.me>
2024-01-11 20:27:30 +0100

    added terraformls

--- .config/nvim/lua/mylsp.lua
+++ .config/nvim/lua/mylsp.lua
@@ -233,7 +233,7 @@ require("typescript").setup({
 
 -- LSP Servers: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 -- Basic setup
-local servers = { 'pyright', 'luau_lsp', 'eslint', 'ccls', "taplo", "bashls", "cssls", "jsonls", "html", "lua_ls" }
+local servers = { 'pyright', 'luau_lsp', 'eslint', 'ccls', "taplo", "bashls", "cssls", "jsonls", "html", "lua_ls", "terraformls" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
