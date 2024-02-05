d6949d3970a95e181cf26dbec4a245a58513d544
Lyr <lyr-7d1h@pm.me>
2024-02-05 17:57:12 +0100

    added prettier

--- .config/nvim/lua/mylsp.lua
+++ .config/nvim/lua/mylsp.lua
@@ -177,68 +177,28 @@ lspconfig.ltex.setup {
 }
 
 -- Setup prettier
--- require("null-ls").setup({
---   on_attach = function(client, bufnr)
---     if client.supports_method("textDocument/formatting") then
---       vim.keymap.set("n", "<Leader>f", function()
---         vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
---       end, { buffer = bufnr, desc = "[lsp] format" })
---
---       -- format on save
---       vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
---       vim.api.nvim_create_autocmd(event, {
---         buffer = bufnr,
---         group = group,
---         callback = function()
---           vim.lsp.buf.format({ bufnr = bufnr, async = async })
---         end,
---         desc = "[lsp] format on save",
---       })
---     end
---
---     if client.supports_method("textDocument/rangeFormatting") then
---       vim.keymap.set("x", "<Leader>f", function()
---         vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
---       end, { buffer = bufnr, desc = "[lsp] format" })
---     end
---   end,
--- })
--- local prettier = require("prettier")
--- prettier.setup({
---   bin = 'prettierd',
---   filetypes = {
---     "css",
---     "graphql",
---     "html",
---     "javascript",
---     "javascriptreact",
---     "json",
---     "less",
---     "markdown",
---     "scss",
---     "typescript",
---     "typescriptreact",
---     "yaml",
---   },
--- })
---
--- lspconfig["pylsp"].setup {
---   on_attach = on_attach,
---   capabilities = capabilities,
---   plugins = {
---     black = {
---       enabled = true
---     },
---     mypy = {
---       enabled = true,
---       live_mode = true,
---       strict = true
---     },
---     isort = {
---       enabled = true
---     }
---   }
--- }
+local null_ls = require("null-ls")
+null_ls.setup({
+  on_attach = on_attach
+})
+local prettier = require("prettier")
+prettier.setup({
+  bin = 'prettierd',
+  filetypes = {
+    "css",
+    "graphql",
+    "html",
+    "javascript",
+    "javascriptreact",
+    "json",
+    "less",
+    "markdown",
+    "scss",
+    "typescript",
+    "typescriptreact",
+    "yaml",
+  },
+})
 
 require("typescript").setup({
   disable_commands = false, -- prevent the plugin from creating Vim commands
