7c83c2b10bbbc361d244cc21cba8136ca6adfdef
Lyr <lyr-7d1h@pm.me>
2025-03-31 09:30:51 +0200

    Update local_mirror script to fetch resources efficiently

--- bin/local_mirror.sh
+++ bin/local_mirror.sh
@@ -1,23 +1,36 @@
 #!/usr/bin/env bash
 #
 # Fetch all resources of a website to be able to view it locally
-# Get cookies.txt using ~/bin/extract_cookies \
-#
-wget \
-	--recursive \
-	--mirror \
-	--page-requisites \
-	--convert-links \
-	--no-parent \
-	--adjust-extension \
-	--span-hosts \
-	--wait="2" \
-	--random-wait \
-	--no-clobber \
-	--timestamping \
-	-e robots=off \
-	--keep-session-cookies \
-	--user-agent="Mozilla/5.0 (X11; Linux x86_64; rv:136.0) Gecko/20100101 Firefox/136.0" \
-	--load-cookies cookies.txt \
-	"${@}"
 
+cookies=$(mktemp)
+~/bin/extract_cookies > "${cookies}"
+
+if [[ "$1" == *.json ]]; then
+  # Extract URLs from the JSON file using jq
+  urls=$(jq -r '.[]' "$1")
+  shift # Remove the JSON file argument
+else
+  # Use the provided arguments as URLs
+  urls="$@"
+fi
+
+for url in ${urls}; do
+  wget \
+    --page-requisites \
+    --convert-links \
+    --no-parent \
+    --level 0 \
+    --adjust-extension \
+	--random-wait=2 \
+	--wait 2 \
+    --span-hosts \
+    --no-clobber \
+    --timestamping \
+    -e robots=off \
+    --keep-session-cookies \
+    --user-agent="Mozilla/5.0 (X11; Linux x86_64; rv:136.0) Gecko/20100101 Firefox/136.0" \
+    --load-cookies "${cookies}" \
+    "${url}"
+done
+
+rm -f "${cookies}"]]
